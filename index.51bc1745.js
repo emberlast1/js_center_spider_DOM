f71e788aff4e541cb85d70649dcaae35c3ca8821
Ostap Hedz <lordanvil18@gmail.com>
2024-08-06 10:54:27 +0200

    make build

---
+++ index.51bc1745.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientHeight,n=t.clientWidth,i=e.getBoundingClientRect().height,l=e.getBoundingClientRect().width;e.style.top="".concat((c-i)/2,"px"),e.style.left="".concat((n-l)/2,"px");
+//# sourceMappingURL=index.51bc1745.js.map
